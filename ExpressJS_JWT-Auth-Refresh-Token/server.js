414e2f40ee2d0bd722fac3bd36d3d62be096edb8
linh2020 <cdev.linh@gmail.com>
2024-06-10 16:38:05 -0700

    Added Views folder

--- ExpressJS_JWT-Auth-Refresh-Token/server.js
+++ ExpressJS_JWT-Auth-Refresh-Token/server.js
@@ -1,17 +1,23 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 const app = express();
 
+const PATH = require("path");
 const PORT = process.env.PORT || 5000;
 
 // parsing
 app.use(express.json());
 
+app.get("^/$|/index(.html)?", (req, res) => {
+  //   res.status(200).json({ msg: "JWT Authentication With Refresh Tokens" });
+  //   res.status(200).sendFile("./views/index.html", { root: __dirname });
+  res.status(200).sendFile(path.join(__dirname, "views", "index.html"));
+});
 
-
-app.get("/", (req, res) => {
-  res.status(200).json({ msg: "JWT Authentication With Refresh Tokens" });
+app.get("^/$|/404(.html)?", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT, () =>
