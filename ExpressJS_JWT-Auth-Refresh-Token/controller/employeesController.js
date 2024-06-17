0d025daaf1ddd0c5ff1e058cfbd1a348e0aefd3c
linh2020 <cdev.linh@gmail.com>
2024-06-16 23:04:01 -0700

    updated updateEmployeeController

--- ExpressJS_JWT-Auth-Refresh-Token/controller/employeesController.js
+++ ExpressJS_JWT-Auth-Refresh-Token/controller/employeesController.js
@@ -28,15 +28,29 @@ const createNewEmployee = (req, res) => {
 
   data.setEmployees([...data.employees, newEmployee]);
   // data.setEmployees(newEmployee);
-  res.status(201).json({ status: "success", employees: data.employees });
+  res.status(201).json({ status: "Success", employees: data.employees });
 };
 
 const updateEmployee = (req, res) => {
-  res.status(200).json({
-    id: req.body.id,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-  });
+  const employeeIndex = data.employees.findIndex(
+    (emp) => emp.id == req.body.id
+  );
+
+  if (employeeIndex === -1)
+    return res.status(400).json({
+      status: "Failed",
+      message: `Employee ID ${req.body.id} not found`,
+    });
+
+  const updateEmployee = { ...data.employees[employeeIndex] };
+  console.log(updateEmployee);
+
+  if (req.body.firstName) updateEmployee.firstName = req.body.firstName;
+  if (req.body.lastName) updateEmployee.lastName = req.body.lastName;
+
+  data.employees[employeeIndex] = updateEmployee;
+
+  res.status(200).json({ status: "Success", employees: data.employees });
 };
 
 const deleteEmployee = (req, res) => {
